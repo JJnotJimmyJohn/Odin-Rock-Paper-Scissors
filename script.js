984854fe392a2026d793ff8bb1385affa7e5cf71
jianjin <jxj362@case.edu>
2023-02-04 22:36:39 -0800

    Finished the small project

--- script.js
+++ script.js
@@ -21,18 +21,18 @@ function getResult(playerSelection,computerSelection) {
     if (playerSelection==computerSelection) {
         return [0,0,"Tie!! Your "+ playerSelection + " ties with AI's " + computerSelection];
     } else if ((playerSelection=="rock")&(computerSelection=="scissors")) {
-        return youWinStatement(playerSelection,computerSelection);
+        return [1,0,youWinStatement(playerSelection,computerSelection)];
     } else if ((playerSelection=="paper")&(computerSelection=="rock")) {
-        return youWinStatement(playerSelection,computerSelection);
+        return [1,0,youWinStatement(playerSelection,computerSelection)];
     } else if ((playerSelection=="scissors")&(computerSelection=="paper")) {
-        return youWinStatement(playerSelection,computerSelection);
+        return [1,0,youWinStatement(playerSelection,computerSelection)];
     } else {
         return [0,1,"You lose!! Your "+ playerSelection + " lost to AI's " + computerSelection];
     }
 }
 
 function youWinStatement(playerSelection,computerSelection) {
-    return [1,0,"You win!! Your "+ playerSelection + " beats AI's " + computerSelection];
+    return "You win!! Your "+ playerSelection + " beats AI's " + computerSelection;
 }
 
 function playRound(playerSelection, computerSelection) {
@@ -43,13 +43,25 @@ function playRound(playerSelection, computerSelection) {
 
     result = getResult(playerSelection,computerSelection)
     
-    console.log(result[2],'!! you get ',result[0], ' point!');
+    console.log(result[2],'!! you get '+result[0]+ ' point!');
 
     return result
   }
 
-function game(round=5) {
-    for (let i=0;i<round;i++) {
-        playRound(getPlayerChoice(),getComputerChoice())
+function game(nRound=5) {
+    let yourPoint=0
+    let aiPoint=0
+    for (let i=0;i<nRound-1;i++) {
+        roundResult=playRound(getPlayerChoice(),getComputerChoice())
+        yourPoint=yourPoint+roundResult[0]
+        aiPoint=aiPoint+roundResult[1]
+        console.log('Game--'+(i+1),'\nCurrent Scores: '+yourPoint+' : '+ aiPoint)
+    }
+    if (yourPoint>aiPoint) {
+        console.log(': ) You won!!!!')
+    } else if (yourPoint<aiPoint) {
+        console.log(':( You Lost!!!!')
+    } else {
+        console.log('Thats a tie!!!!')
     }
 }
