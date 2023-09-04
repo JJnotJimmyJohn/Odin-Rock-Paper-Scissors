1334c5412df9de575f6a5bd3bf1ea44db9080fc5
jianjin <jxj362@case.edu>
2023-09-03 23:48:22 -0700

    [FEA] buttons added
    Now instead of typing, clicking buttons is all we need.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 `use strict`
 
+import Math
+
 const choices=['rock','paper','scissors']
 
 function getComputerChoice() {
@@ -43,25 +45,50 @@ function playRound(playerSelection, computerSelection) {
 
     result = getResult(playerSelection,computerSelection)
     
-    console.log(result[2],'!! you get '+result[0]+ ' point!');
+    let body = document.querySelector('body')
+    // console.log(body)
+    let result_div = document.createElement("div")
+    result_div.classList.add('result')
+    if (result[0]==1) {
+        result_div.innerText = `-- ${playerSelection} > ${computerSelection}!! ${result[2]}!! you get ${result[0]}  point! --`
+    } else if (result[1]==1) {
+        result_div.innerText = `-- ${playerSelection} < ${computerSelection}!! ${result[2]}!! you get ${result[0]}  point! --`
+    } else {
+        result_div.innerText = `-- ${playerSelection} = ${computerSelection}!! ${result[2]}!! you get ${result[0]}  point! --`
+    }
+    body.append(result_div)
 
     return result
   }
 
-function game(nRound=5) {
-    let yourPoint=0
-    let aiPoint=0
-    for (let i=0;i<nRound-1;i++) {
-        roundResult=playRound(getPlayerChoice(),getComputerChoice())
-        yourPoint=yourPoint+roundResult[0]
-        aiPoint=aiPoint+roundResult[1]
-        console.log('Game--'+(i+1),'\nCurrent Scores: '+yourPoint+' : '+ aiPoint)
-    }
-    if (yourPoint>aiPoint) {
-        console.log(': ) You won!!!!')
-    } else if (yourPoint<aiPoint) {
-        console.log(':( You Lost!!!!')
-    } else {
-        console.log('Thats a tie!!!!')
-    }
+
+function remove_all() {
+    body = document.querySelector('body');
+    result_divs = Array.from(body.querySelectorAll('.result'));
+    result_divs.forEach(element => {
+        console.log('trying to remove')
+        element.remove();
+        console.log('removed')
+    });
 }
+
+let running_score = 0;
+
+const rock_button = document.querySelector('#rock')
+rock_button.addEventListener("click", () => {
+    console.log('Rock is clicked');
+    playRound("rock", getComputerChoice());
+    
+})
+
+const paper_button = document.querySelector('#paper')
+paper_button.addEventListener("click", () => {
+    console.log('Paper is clicked');
+    playRound("paper", getComputerChoice());
+})
+
+const scissor_button = document.querySelector('#scissor')
+scissor_button.addEventListener("click", () => {
+    console.log('Scissor is clicked');
+    playRound("scissor", getComputerChoice());
+})
